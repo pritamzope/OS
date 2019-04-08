dbc62a84151d01385d794ef7cf00785c0d1f147b
Pritam Zope <zopepritam444@gmail.com>
2019-04-08 23:27:36 +0530

    Update kernel.h

--- Tic-Tac-Toe/src/kernel.h
+++ Tic-Tac-Toe/src/kernel.h
@@ -11,12 +11,6 @@
 uint16* vga_buffer;
 extern uint32 vga_index;
 
-#define BOX_MAX_WIDTH 78
-#define BOX_MAX_HEIGHT 23
-
-#define BOX_SINGLELINE 1
-#define BOX_DOUBLELINE 2
-
 enum vga_color {
     BLACK,
     BLUE,
