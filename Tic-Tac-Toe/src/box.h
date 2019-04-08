69bb17353cf32e9f234d07db0cb42fcfbde2cab8
Pritam Zope <zopepritam444@gmail.com>
2019-04-08 23:27:54 +0530

    Update box.h

--- Tic-Tac-Toe/src/box.h
+++ Tic-Tac-Toe/src/box.h
@@ -3,6 +3,14 @@
 
 #include "types.h"
 
+
+#define BOX_MAX_WIDTH 78
+#define BOX_MAX_HEIGHT 23
+
+#define BOX_SINGLELINE 1
+#define BOX_DOUBLELINE 2
+
+
 extern void draw_generic_box(uint16, uint16, uint16, uint16, 
                             uint8, uint8, uint8, uint8, 
                             uint8, uint8, uint8, uint8);
