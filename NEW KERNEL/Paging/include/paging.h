37c9daa8d9005a5af14a81ec68af80169a15011e
Pritam Zope <zopepritam444@gmail.com>
2024-10-02 13:09:47 +0530

    Update paging.h

--- NEW KERNEL/Paging/include/paging.h
+++ NEW KERNEL/Paging/include/paging.h
@@ -7,35 +7,6 @@
 #define PAGE_SIZE 4096
 
 
-typedef struct {
-    uint32 present    :1;
-    uint32 read_write         :1;
-    uint32 user       :1;
-    uint32 write_through  :1;
-    uint32 cache      :1;
-    uint32 accessed     :1;
-    uint32 dirty   :1;
-    uint32 page_size  :1;
-    uint32 global     :1;
-    uint32 available  :3;
-    uint32 frame      :20;
-} PAGE_DIRECTORY;
-
-typedef struct {
-    uint32 present    :1;
-    uint32 read_write         :1;
-    uint32 user       :1;
-    uint32 write_through   :1;
-    uint32 cache      :1;
-    uint32 accessed   :1;
-    uint32 dirty      :1;
-    uint32 page_size  :1;
-    uint32 global     :1;
-    uint32 available  :3;
-    uint32 frame      :20;
-} PAGE_TABLE;
-
-
 void paging_init();
 
 void *paging_get_physical_address(void *virtual_addr);
